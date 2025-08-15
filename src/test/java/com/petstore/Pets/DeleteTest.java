a3e3bac1d774e507af5b24aed09234e39e4773a1
Darya <d.zubkova@mail.astondevs.ru>
2025-08-15 19:45:13 +0300

    автотест на изменение имени и статуса животного

---
+++ src/test/java/com/petstore/Pets/DeleteTest.java
@@ -0,0 +1,31 @@
+package com.petstore.Pets;
+
+import com.petstore.TestBase;
+import com.petstore.models.Pet;
+import com.petstore.utils.ApiClient;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeleteTest extends TestBase {
+
+    @Test
+    @DisplayName("Удаление питомца")
+    public void deleteTest() {
+        Pet newPet = testDataGenerator.generatePet();
+        Response createResponse = ApiClient.post("/pet", newPet);
+        Pet createdPet = createResponse.as(Pet.class);
+
+        Response getResponse = ApiClient.get("/pet/" + createdPet.getId());
+        Pet retrievedPet = getResponse.as(Pet.class);
+
+        Response deleteResponse = ApiClient.delete("/pet/" + createdPet.getId());
+
+        assertEquals(HTTP_OK, getResponse.getStatusCode());
+        assertEquals(createdPet.getId(), retrievedPet.getId());
+        assertEquals(HTTP_OK, deleteResponse.getStatusCode());
+    }
+}
